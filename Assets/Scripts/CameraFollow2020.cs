e284e7637fb317a4e99cd765f2c319227533a9e9
adrianpiecek <adrian.piecek@gmail.com>
2023-08-12 07:47:59 +0200

    Sprites and new scripts

---
+++ Assets/Scripts/CameraFollow2020.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow2020 : MonoBehaviour
+{
+
+    public float interpVelocity;
+    public float minDistance;
+    public float followDistance;
+    public float speed;
+
+    public GameObject target;
+    public Vector3 offset;
+    Vector3 targetPos;
+    // Use this for initialization
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
+
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+
+            interpVelocity = targetDirection.magnitude * speed;
+
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformerController : PhysicsObject
+{
+
+    public float maxSpeed = 7;
+    public float jumpTakeOffSpeed = 7;
+
+    private float fJumpPressedRemember = 0;
+    private float fJumpPressedRememberTime = 0.17f;
+    private float fGroundedRemember = 0;
+    private float fGroundedRememberTime = 0.2f;
+
+    private bool m_FacingRight = true;
+    private SpriteRenderer spriteRenderer;
+    //private Animator animator;
+
+    // Use this for initialization
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //animator = GetComponent<Animator>();
+    }
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        fGroundedRemember -= Time.deltaTime;
+        if (grounded)
+        {
+            fGroundedRemember = fGroundedRememberTime;
+        }
+
+        fJumpPressedRemember -= Time.deltaTime;
+        if (Input.GetButtonDown("Jump"))
+        {
+            fJumpPressedRemember = fJumpPressedRememberTime; 
+        }
+
+        if ((fJumpPressedRemember > 0) && (fGroundedRemember > 0))
+        {
+            fJumpPressedRemember = 0;
+            fGroundedRemember = 0;
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (velocity.y > 0)
+            {
+                velocity.y = velocity.y * 0.5f;
+            }
+        }
+
+        // If the input is moving the player right and the player is facing left...
+        if (move.x > 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (move.x < 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
+        // animator.SetBool("grounded", grounded);
+        // animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+        targetVelocity = move * maxSpeed;
+
+        
+    }
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
