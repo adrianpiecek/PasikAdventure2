86a87f87e6f675a6e2e5d29ee7e4c2a532237f49
adrianpiecek <adrian.piecek@gmail.com>
2023-08-12 08:00:09 +0200

    Updated old scripts

--- Assets/Scripts/Old Scripts/Platformer2DUserControl.cs
+++ Assets/Scripts/Old Scripts/Platformer2DUserControl.cs
@@ -1,6 +1,6 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets;
 
 namespace UnityStandardAssets._2D
 {
@@ -25,7 +25,7 @@ namespace UnityStandardAssets._2D
             if (!m_Jump)
             {
                 // Read the jump input in Update so button presses aren't missed.
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+                m_Jump = Input.GetButtonDown("Jump");
             }
         }
          
@@ -33,7 +33,7 @@ namespace UnityStandardAssets._2D
         {
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            float h = Input.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
             m_Jump = false;
